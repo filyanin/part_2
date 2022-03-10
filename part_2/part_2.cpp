b3fe0a3998ed1c3e0344e1b0d57b7b71d70e5dd6
Никита Филянин <famely.FSA@yandex.ru>
2022-03-10 08:56:46 +0300

    Исправлено

--- part_2/part_2.cpp
+++ part_2/part_2.cpp
@@ -6,9 +6,10 @@ public:
 
     MyMatrix(int _xSize, int _ySize, std::string *_info, std::string *_description)
     {
-        int XSize = _xSize;
-        int YSize = _ySize;
-        int* matrix = new int(XSize * YSize);
+        XSize = _xSize;
+        YSize = _ySize;
+
+        matrix = new int(XSize * YSize);
 
         for (int i = 0; i < XSize; i++) {
             for (int j = 0; j < YSize; j++) {
@@ -16,17 +17,31 @@ public:
             }
         }
 
-        std::string* Info = _info;
-        std::string* Description = _description;       
+        Info = new std::string(*_info);
+        Description = new std::string(*_description);
+
+        arrayOfArray = new int* [XSize];
+        
+        for (int i = 0; i < XSize; i++) {
+            arrayOfArray[i] = new int[YSize];
+        }
     }
 
 
     ~MyMatrix()
     {
-        delete this->matrix;
+        delete[] matrix;
         delete Info;
         delete Description;
 
+        for (int i = 0; i < XSize; i++) {
+            delete[] arrayOfArray[i];
+        }
+        delete[] arrayOfArray;
+    }
+
+    int operator()(const int i, const int j) {
+        return *(matrix + i * XSize + j);
     }
 
     MyMatrix& operator=(MyMatrix& _myMatrix) {
@@ -40,7 +55,21 @@ public:
 private:
     int XSize;
     int YSize;
-    int* matrix;
+    // Матрица представлена как одномерный массив, где каждая следующая строка является продолжением предыдущей, т.е.
+    /*
+    Матрица вида:
+    123
+    321
+    111
+    Хранится в памяти, как 123321111, а доступ по индексам матрицы осуществляется с помощью вычисления положения ячейки в общей цепочке
+    Для наглядности сделал перегрузку оператора () для доступа к элементам массива по индексам
+    */
+    int* matrix; 
+
+    /*Матрица представленна как "массив масивов"*/
+    int** arrayOfArray;
+
+
     std::string* Info;
     std::string* Description;
 };
